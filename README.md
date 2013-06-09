2e8a8c89876a5c01a4aebd2e5f5646fa7aa8b13e
MunkiPhD <munkiphd@gmail.com>
2013-06-08 22:31:09 -0400

    refactored and added notes to readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 Code Kata Four
 ==========
 http://codekata.pragprog.com/2007/01/kata_four_data_.html
+
+Questions Post-Exercise
+---------
+**To what extent did the design decisions you made when writing the original programs make it easier or harder to factor out common code?**
+ - I made the reading of the dat file an external class, which allowed for ease of use when it was needed in the two parts
+
+**Was the way you wrote the second program influenced by writing the first?**
+ - Yes. The dat file followed the same format and the analysis of the data was the same. The only real difference was which columns had to be analyzed and taking into account absolute values
+
+**Is factoring out as much common code as possible always a good thing? Did the readability of the programs suffer because of this requirement? How about the maintainability?**
+ - ALWAYS factoring out common code will sometimes present an issue. If you look at the final state, the two methods that invoke the analysis essentially do the same thing. Even the printing to the screen is the same - it's just displayed differently. You could say I didn't follow the directions by refactoring all the common code - and it's true. I stopped where I believed the program was readable and maintainable. You could refactor it a bit more to have a SoccerDataAnalysis and WeatherDataAnalysis classes or SmallestSpreadAnalysis classes, etc, but then you start to complicate the system for no reason. You could say that doing so would break the common motto of TDD that's along the lines of 'do the least you could do to get the tests to pass' (which, ironically, I wrote no tests for this program, lol).
+
--- data_munging.rb
+++ data_munging.rb
@@ -1,29 +1,13 @@
 require_relative 'dat_reader'
 
 class DataMunging
+	#
+	# Finds the smallest spread in the soccer scores
+	#
 	def self.find_smallest_spread_soccer
 		data = get_data('football.dat')
-		team_with_smallest_spread = nil
-		smallest_spread = 9999 # cant have this big of a spread, so set it really high
 
-		results = []
-
-		for i in 3..data.length - 2
-			unless data[i][1].nil? # get rid of the row of all lines. it's parsed into the first location, so check to see if the second location of the array is empty
-				team = data[i][1]
-				wins = data[i][6].to_f
-				losses = data[i][8].to_f
-				difference = (wins - losses).abs # get the absolute value so that we have the difference
-
-				if difference < smallest_spread
-					results = []
-					results << [team, wins, losses, difference]
-					smallest_spread = difference
-				elsif difference == smallest_spread # going to check for equality in order to deal with ties
-					results << data[i]
-				end
-			end
-		end
+		results = smallest_spread(data, 1, 6, 8)
 
 		puts "Smallest Spread Results: "
 		results.each { |item|
@@ -31,35 +15,55 @@ class DataMunging
 		}
 	end
 
-
+	#
+	# Finds the smallest spread in temperature from the specified file
+	#
 	def self.find_smallest_spread_weather
 		# get the data from the file
 		data = get_data('weather.dat')
-		day_with_smallest_spread = 0
-		smallest_spread = 9999 # this spread could never exist, so we'll use it as an upper bound
 
-		# we know that the data starts at line 3, so start there until the end
-		for i in 3..data.length - 2 # we want to cut out the month totals
-			day = data[i][0]
-			max_temp = data[i][1].to_f
-			min_temp = data[i][2].to_f
-
-			diff = max_temp - min_temp
-
-			puts "day: #{day}   min: #{min_temp}   max: #{max_temp}   diff: #{diff}"
-
-			if diff < smallest_spread
-				day_with_smallest_spread = day
-				smallest_spread = diff
-			end
-		end
-
-		puts "--- Day with smallest spread: #{day_with_smallest_spread} with a spread of #{smallest_spread} degrees"
+		results = smallest_spread(data, 0, 1, 2)
+		#puts "--- Day with smallest spread: #{day_with_smallest_spread} with a spread of #{smallest_spread} degrees"
+		puts "Smallest Spread Results: "
+		results.each { |item|
+			puts "Day: #{item[0]} High:#{item[1]} Low:#{item[2]}   Difference: #{item[3]}"
+		}
 	end
 
 	private
 
+	#
+	# Gets an array of the data from the specified file
+	#
 	def self.get_data(file_name)
 		return DatReader.read_file(file_name)
+	end
+
+
+	#
+	# Returns a result array with the name, the maximum, minimum, and difference of the data with the smallest spread
+	#
+	def self.smallest_spread(data, name_col, max_col, min_col)
+		smallest_spread = 9999 # cant have this big of a spread, so set it really high
+		results = []
+
+		for i in 3..data.length - 2
+			unless data[i][1].nil? # get rid of the row of all lines. it's parsed into the first location, so check to see if the second location of the array is empty
+				name = data[i][name_col]
+				max = data[i][max_col].to_f
+				min = data[i][min_col].to_f
+				difference = (max-min).abs # get the absolute value so that we have the difference (the min might not necessarily be less than the max)
+
+				if difference < smallest_spread
+					results = []
+					results << [name, max, min, difference]
+					smallest_spread = difference
+				elsif difference == smallest_spread # going to check for equality in order to deal with ties
+					results << data[i]
+				end
+			end
+		end
+
+		results
 	end
 end
