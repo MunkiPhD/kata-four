3ef8ca4145fac1a6e0d42ab4e4e820c0d1a89933
MunkiPhD <munkiphd@gmail.com>
2013-06-08 22:12:25 -0400

    added football.dat data parsing and analysis - part 2 of kata

--- data_munging.rb
+++ data_munging.rb
@@ -1,14 +1,44 @@
 require_relative 'dat_reader'
 
 class DataMunging
-	def self.find_smallest_spread
+	def self.find_smallest_spread_soccer
+		data = get_data('football.dat')
+		team_with_smallest_spread = nil
+		smallest_spread = 9999 # cant have this big of a spread, so set it really high
+
+		results = []
+
+		for i in 3..data.length - 2
+			unless data[i][1].nil? # get rid of the row of all lines. it's parsed into the first location, so check to see if the second location of the array is empty
+				team = data[i][1]
+				wins = data[i][6].to_f
+				losses = data[i][8].to_f
+				difference = (wins - losses).abs # get the absolute value so that we have the difference
+
+				if difference < smallest_spread
+					results = []
+					results << [team, wins, losses, difference]
+					smallest_spread = difference
+				elsif difference == smallest_spread # going to check for equality in order to deal with ties
+					results << data[i]
+				end
+			end
+		end
+
+		puts "Smallest Spread Results: "
+		results.each { |item|
+			puts "#{item[0]} W:#{item[1]} L:#{item[2]}   Difference: #{item[3]}"
+		}
+	end
+
+
+	def self.find_smallest_spread_weather
 		# get the data from the file
 		data = get_data('weather.dat')
 		day_with_smallest_spread = 0
 		smallest_spread = 9999 # this spread could never exist, so we'll use it as an upper bound
 
 		# we know that the data starts at line 3, so start there until the end
-		results = []
 		for i in 3..data.length - 2 # we want to cut out the month totals
 			day = data[i][0]
 			max_temp = data[i][1].to_f
